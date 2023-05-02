dd941b5af35d374636925d5f763a2a18d71f6166
Hyunul <jungwj1023@naver.com>
2023-05-02 20:19:00 +0900

    '0502_update'

--- app.js
+++ app.js
@@ -1,58 +1,22 @@
-const express = require("express");
-const session = require("express-session");
-const bodyParser = require("body-parser");
-const FileStore = require("session-file-store")(session);
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
 
-var authRouter = require("./lib_login/auth.js");
-var authCheck = require("./lib_login/authCheck.js");
-var template = require("./lib_login/template.js");
+var indexRouter = require("./routes/index");
+var authRouter = require("./routes/auth");
 
-const app = express();
-const port = 3000;
+var app = express();
+var port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(
-  session({
-    secret: "~~~", // 원하는 문자 입력
-    resave: false,
-    saveUninitialized: true,
-    store: new FileStore(),
-  })
-);
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  if (!authCheck.isOwner(req, res)) {
-    // 로그인 안되어있으면 로그인 페이지로 이동시킴
-    res.redirect("/auth/login");
-    return false;
-  } else {
-    // 로그인 되어있으면 메인 페이지로 이동시킴
-    res.redirect("/main");
-    return false;
-  }
-});
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-// 인증 라우터
+app.use("/", indexRouter);
 app.use("/auth", authRouter);
-
-// 메인 페이지
-app.get("/main", (req, res) => {
-  if (!authCheck.isOwner(req, res)) {
-    // 로그인 안되어있으면 로그인 페이지로 이동시킴
-    res.redirect("/auth/login");
-    return false;
-  }
-  // var html = template.HTML(
-  //   "Welcome",
-  //   `<hr>
-  //       <h2>메인 페이지에 오신 것을 환영합니다</h2>
-  //       <p>로그인에 성공하셨습니다.</p>`,
-  //   authCheck.statusUI(req, res)
-  // );
-  else {
-    res.sendFile(__dirname + "/agnes/index.html");
-  }
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
